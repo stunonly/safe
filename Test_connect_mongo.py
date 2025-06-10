153ccc8da1c886789409a1323e0b504e161f78d6
stunonly <43926863+stunonly@users.noreply.github.com>
2025-06-10 09:57:08 +0700

    Add files via upload
    first commit

---
+++ Test_connect_mongo.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+
+import pandas as pd
+client = MongoClient("mongodb+srv://sittichaitantanasiwakul:fAnNVJmPpCi9eW8T@clustersafe.fncdgxk.mongodb.net/")
+
+db = client["mongodbVSCodePlaygroundDB"]
+collection = db["sales"]
+
+#for doc in collection.find():
+    #print(doc)
+
+data = list(collection.find())
+df = pd.DataFrame(data)
+print(df.head())
+
+
+
