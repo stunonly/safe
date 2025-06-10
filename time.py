ced06d880e6d474a8e9d482c09460579a2bd97fe
stunonly <safe.77@hotmail.com>
2025-06-10 10:29:59 +0700

    first committed

---
+++ time.py
@@ -0,0 +1,16 @@
+import time
+
+def simulate_clock(hours=3):
+    total_seconds = hours * 60 * 60
+    for sec in range(total_seconds):
+        hr = sec // 3600
+        min = (sec % 3600) // 60
+        s = sec % 60
+        print(f"{hr:02} Hour {min:02} Min {s:02} Sec")
+        time.sleep(1)
+
+simulate_clock()    
+
+
+### test safe 
+
